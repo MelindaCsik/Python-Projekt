b3a49ccaccc5a05d381dacde31951840b12461c2
lenartaron <83168006+lenartaron@users.noreply.github.com>
2022-06-05 10:48:09 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,29 +1,14 @@
-
-from tkinter import Button
+from tkinter import TOP, Button
 from tkinter.tix import Tk
 from turtle import left, right
 
 
 def pitagorasz():
-    #megkérdi a felhaSZNÁLÓT hogy átfogót vagy befogót akar számolni
-    button = Button(pitagorasz, text="Szekrény", command = atfogo)
-    #Itt a gomb kinézeti beállításai
-    button.config(font = ("Ink Free", 25, "bold"))
-    button.config(bg = "#787878")
-    button.config(fg = "#ffffff")
-    #A gomb elhelyezkedése
-    button.pack(side = left)
-    button = Button(pitagorasz, text="Szekrény", command = befogo)
-    #Itt a gomb kinézeti beállításai
-    button.config(font = ("Ink Free", 25, "bold"))
-    button.config(bg = "#787878")
-    button.config(fg = "#ffffff")
-    #A gomb elhelyezkedése
-    button.pack(side = right)
+   print("helló")
 
 #----CSOPORT----
 #A gomb csoportjai amivel később dolgozhatunk
-root = Tk  
+root = Tk()
 
 button1 = Button(root, text="Szekrény", command = pitagorasz)
 #Itt a gomb kinézeti beállításai
@@ -31,7 +16,7 @@ button1.config(font = ("Ink Free", 25, "bold"))
 button1.config(bg = "#787878")
 button1.config(fg = "#ffffff")
 #A gomb elhelyezkedése
-button1.pack(side = right)
+button1.pack(side = TOP)
     
 #----MEGJELENÍTÉS----   
 #Megjeleniti a gombokat/gombot   
