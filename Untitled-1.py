cb62926da5427a5b5d622ce66ef24c4ccf2ac2a3
lenartaron <83168006+lenartaron@users.noreply.github.com>
2022-06-05 10:23:13 +0200

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,17 +1,37 @@
-def open():
-    #Ismétel (ha elrontja a felhasználó akkor is úrja próbálhatja)
-    while True:
-        megold1 = input("Megoldás: ")
-        #Ha sikerült eltalálnia akkor tovább jut
-        if megold1 == "auto" or megold1 == "Auto" or megold1 == "autó" or megold1 == "Autó" or megold1 == "kocsi" or megold1 == "Kocsi":
-            print()
-            print()
-            code1 = random.randint(1,3)
-            print( code1 )    
-            break
-        
-        else:
-            print()
-            print()
-            print()
-            print()
+
+from tkinter.tix import Tk
+from turtle import left, right
+
+
+def pitagorasz():
+    #megkérdi a felhaSZNÁLÓT hogy átfogót vagy befogót akar számolni
+    button = Button(pitagorasz, text="Szekrény", command = atfogo)
+    #Itt a gomb kinézeti beállításai
+    button.config(font = ("Ink Free", 25, "bold"))
+    button.config(bg = "#787878")
+    button.config(fg = "#ffffff")
+    #A gomb elhelyezkedése
+    button.pack(side = left)
+    button = Button(pitagorasz, text="Szekrény", command = befogo)
+    #Itt a gomb kinézeti beállításai
+    button.config(font = ("Ink Free", 25, "bold"))
+    button.config(bg = "#787878")
+    button.config(fg = "#ffffff")
+    #A gomb elhelyezkedése
+    button.pack(side = right)
+
+#----CSOPORT----
+#A gomb csoportjai amivel később dolgozhatunk
+root = Tk  
+
+button1 = Button(root, text="Szekrény", command = pitagorasz)
+#Itt a gomb kinézeti beállításai
+button1.config(font = ("Ink Free", 25, "bold"))
+button1.config(bg = "#787878")
+button1.config(fg = "#ffffff")
+#A gomb elhelyezkedése
+button1.pack(side = right)
+    
+#----MEGJELENÍTÉS----   
+#Megjeleniti a gombokat/gombot   
+root.mainloop()
